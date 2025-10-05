2833301b8b91f11fe3a59b5a627fdf7b0eecbdb7
김주인 <jooinkimmac@gimju-in-ui-MacBookPro.local>
2025-10-05 12:16:30 +0200

    최종 디자인 수정 및 기본 데이터 추가

--- server.js
+++ server.js
@@ -806,4 +806,3 @@ app.get("/download-tx", (req, res) => {
 // -------- 서버 시작 --------
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("listening on http://0.0.0.0:" + PORT));
-```
